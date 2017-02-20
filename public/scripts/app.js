4bebb0478bb5cbd9ce136911f9706aff563a718d
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-02-20 21:46:48 +0000

    added hover over each tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function(){
+  $('.tweet').on('mouseenter', function(){
+    $(this).find('footer').find('.helperIconsDiv').removeClass('noDisplay');
+    console.log('lul')
+  });
+  $('.tweet').on('mouseleave', function(){
+    $(this).find('footer').find('.helperIconsDiv').addClass('noDisplay');
+  });
+});
